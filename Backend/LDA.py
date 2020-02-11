062cf2bc4f4dcc63d25e2f8dc17be49a136d21eb
tehzwen <zachthm@gmail.com>
2020-02-10 17:52:57 -0700

    Hook up LDA script to golang backend

---
+++ Backend/LDA.py
@@ -0,0 +1,68 @@
+import nltk
+import sys
+import time
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import PorterStemmer
+from gensim import corpora, models
+import gensim
+import requests
+
+def analyzeTopic(value, stopWords):
+
+    tokenizer = RegexpTokenizer(r'\w+')
+
+    # create English stop words list
+
+    # Create p_stemmer of class PorterStemmer
+    p_stemmer = PorterStemmer()
+
+    # list for tokenized documents in loop
+    texts = []
+
+    # Convert strings to raw text
+    raw = value.lower()
+    tokens = tokenizer.tokenize(raw)
+
+    # remove stop words from tokens
+    stopped_tokens = [i for i in tokens if not i in stopWords]
+
+    # stem tokens
+    stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+
+    # add tokens to list
+    texts.append(stemmed_tokens)
+    # turn our tokenized documents into a id <-> term dictionary
+    dictionary = corpora.Dictionary(texts)
+
+    # convert tokenized documents into a document-term matrix
+    corpus = [dictionary.doc2bow(text) for text in texts]
+
+    # generate LDA model
+    ldamodel = gensim.models.ldamodel.LdaModel(
+        corpus, num_topics=1, id2word=dictionary, passes=20)
+
+    val = {
+        "topic" : ldamodel.print_topic(topn=1, topicno=0)
+    }
+
+    print(ldamodel.print_topic(topn=1, topicno=0))
+
+    #print(ldamodel.print_topic(topn=1, topicno=0))
+
+
+def main():
+    args = sys.argv
+    if (len(args) <= 1):
+        print("Usage: ./LDA.py <value>")
+        exit(1)
+
+    if (stopwords):
+        nStopWords = stopwords.words('english')
+        analyzeTopic(args[1], nStopWords)
+    else:
+        nltk.download('stopwords', quiet=True)
+        nStopWords = stopwords.words('english')
+        analyzeTopic(args[1], nStopWords)
+
+main()
--- pythonScriptReddit/LDA.py
+++ pythonScriptReddit/LDA.py
@@ -1,4 +1,6 @@
 import nltk
+import sys
+import time
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.porter import PorterStemmer
@@ -6,53 +8,52 @@ from gensim import corpora, models
 import gensim
 import requests
 
-tokenizer = RegexpTokenizer(r'\w+')
 
-# create English stop words list
-nltk.download('stopwords')
-stopwords = stopwords.words('english')
+def analyzeTopic(value, stopWords):
 
-# Create p_stemmer of class PorterStemmer
-p_stemmer = PorterStemmer()
+    tokenizer = RegexpTokenizer(r'\w+')
 
-# create sample documents
-# long = requests.get('http://rare-technologies.com/the_matrix_synopsis.txt').text
-long = (
-    "I got my dog after a suicide attempt that landed me in the hospital for a while. I was beyond depressed, "
-    "abusing alcohol, and I just had a thought that I wanted a dog.I was scrolling through adoption ads, going into "
-    "random pet stores, checking all the rescues. And then one day I saw my pup in a little cage with some other "
-    "dogs. He wasn’t even three pounds and he didn’t give a FUCK that anyone was looking at him. He was just chilling "
-    "and doing his own thing, all the other puppies were jumping around.I held him in my arms and I just had this "
-    "warm feeling fill my entire chest. It was the first time I’d really felt like that in a long time. It’s been 2.5 "
-    "years now and I’m not depressed anymore. My life has done a complete 180 and my dog is currently sitting here "
-    "grabbing my hand like “excuse me you’re welcome now pet me”. Any time the thought that life isn’t going my way "
-    "comes, I look at him and remember that everything has to be okay bc he needs me.So I agree, the world’s best "
-    "antidepressant! "
-)
+    # create English stop words list
 
-short = "Donald Trump does not have the values of America. he continuously damages our way of life. "
-# list for tokenized documents in loop
-texts = []
+    # Create p_stemmer of class PorterStemmer
+    p_stemmer = PorterStemmer()
 
-# Convert strings to raw text
-raw = short.lower()
-tokens = tokenizer.tokenize(raw)
+    # list for tokenized documents in loop
+    texts = []
 
-# remove stop words from tokens
-stopped_tokens = [i for i in tokens if not i in stopwords]
+    # Convert strings to raw text
+    raw = value.lower()
+    tokens = tokenizer.tokenize(raw)
 
-# stem tokens
-stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+    # remove stop words from tokens
+    stopped_tokens = [i for i in tokens if not i in stopWords]
 
-# add tokens to list
-texts.append(stemmed_tokens)
-# turn our tokenized documents into a id <-> term dictionary
-dictionary = corpora.Dictionary(texts)
+    # stem tokens
+    stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
 
-# convert tokenized documents into a document-term matrix
-corpus = [dictionary.doc2bow(text) for text in texts]
+    # add tokens to list
+    texts.append(stemmed_tokens)
+    # turn our tokenized documents into a id <-> term dictionary
+    dictionary = corpora.Dictionary(texts)
 
-# generate LDA model
-ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=1, id2word=dictionary, passes=20)
+    # convert tokenized documents into a document-term matrix
+    corpus = [dictionary.doc2bow(text) for text in texts]
 
-print(ldamodel.print_topics(num_topics=1, num_words=1))
+    # generate LDA model
+    ldamodel = gensim.models.ldamodel.LdaModel(
+        corpus, num_topics=1, id2word=dictionary, passes=20)
+
+    print(ldamodel.print_topics(num_topics=1, num_words=1))
+
+
+def main():
+    nltk.download('stopwords', quiet=True)
+    nStopWords = stopwords.words('english')
+
+    while(True):
+        time.sleep(0.2)
+        data = sys.stdin.readlines()
+        if (len(data) > 0):
+            analyzeTopic(data[0], nStopWords)
+
+main()
